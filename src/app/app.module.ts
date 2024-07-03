478925461c7ade4f2fbdc9e08fe6374359aa7fdf
Ayush Maurya <ayush.maurya@dhwaniris.com>
2024-07-04 00:08:53 +0530

    saving

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes : Routes = [
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'products',
+    component:ProductsComponent
+  },
+  {
+    path:'reviews',
+    component:ReviewsComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    ProductsComponent,
+    ReviewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
